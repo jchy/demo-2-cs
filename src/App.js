65ed8425800bccf0d1fe7743baacf2c6485090a5
Jaswant Chaudhary <91217491+jchy@users.noreply.github.com>
2021-11-30 18:10:48 +0530

    styled component dependency has been added It is an example of styled components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
 import "./styles.css";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  padding: 2rem;
+  border: 1px solid red;
+
+  h1 {
+    color: blue;
+    font-size: 20px;
+  }
+
+  h2 {
+    color: red;
+    @media all and (max-width: 620px) {
+      color: red;
+    }
+  }
+`;
+
+const A = styled.a`
+  text-decoration: none;
+  font-size: 24px;
+  color: ${(props) => (props.color === "aqua" ? "aqua" : "red")};
+`;
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <>
+      <Wrapper className="App">
+        <A color="aqua" href="https://www.google.com">
+          Goto Google.com
+        </A>
+        <h1>Hello CodeSandbox</h1>
+        <h2>Start editing to see some magic happen!</h2>
+      </Wrapper>
+    </>
   );
 }
